4da79a784dabd275f2bec2edd174acd970acb5ca
Christian Lutz <doncmedia@gmail.com>
2012-04-05 12:17:57 +0200

    First Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+Lightbox automation for the JQuery-Plugin colorbox from Jack Moore (http://www.jacklmoore.com/colorbox).
+
+It fetches files from a folder and generates the HTML-Markup and thumbnails.
---
+++ index.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="en">
+<head>
+  	<meta charset="utf-8"/>
+	<title>lightbox automation</title>
+	<link rel="stylesheet" href="css/colorbox.css" type="text/css"/>
+	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+	<script type="text/javascript" src="lib/jquery.colorbox-min.js"></script>
+	</head>
+<body>
+	<?php include 'includes/lightbox.php'; ?>
+</body>
+	<script type="text/javascript">$(document).ready(function() {
+		$('a.gallery').colorbox({rel:'gal', width:'500px'});
+	});
+	</script>
+</html>
