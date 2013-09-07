269e51788400734c33d132d8933f7e3804257d16
Christian Lutz <doncmedia@gmail.com>
2013-09-07 13:18:37 +0200

    Update index.php
    Update jQuery

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
   	<meta charset="utf-8"/>
 	<title>lightbox automation</title>
 	<link rel="stylesheet" href="css/colorbox.css" type="text/css"/>
-	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
 	<script type="text/javascript" src="lib/jquery.colorbox-min.js"></script>
 	</head>
 <body>
